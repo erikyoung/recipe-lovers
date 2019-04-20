410d250e16e73e92b67dd00588548906f4ea6e75
erikyoung <erik.young@rmit.edu.vn>
2019-04-21 00:43:16 +0700

    starting recipes project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p className="text-slanted">Hello from our recipe App</p>
+      </div>
+    );
+  }
+}
+
+export default App;
