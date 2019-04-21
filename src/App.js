df2a59a82397ec12ab583492f7977665aecbd4d1
erikyoung <erik.young@rmit.edu.vn>
2019-04-21 11:16:16 +0700

    added a home hero image to the screen

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 
+
 class App extends Component {
   render() {
     return (
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,20 @@
 /* Core */
 import React, { Component } from 'react';
+import Header from '../components/Header'; 
+import  { Link } from 'react-router-dom'; 
 
 
 
 export default class Home extends Component {
   render() {
     return (
-      <h1>Welcome to the Home Component</h1>
+    <Header title="Amazing Recipes">
+	    <Link to="recipes" className="text-uppercase btn btn-secondary btn-lg mt-3" title="amazing recipes">
+	    	Search Recipes
+	    </Link>
+    </Header>
+      
+
     );
   }
 }
